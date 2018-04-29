3af4cf4d5d8b2ace5e99bb7255f823603b883661
uoon.dev <uoon.dev@gmail.com>
2018-04-29 17:25:18 +0900

    create stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>The Stock - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-panel-heading">
+        <h3 class="panel-title">
+          NAME
+          <small>(Price: PRICE)</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+                  type="number"
+                  class="form-control"
+                  placeholder="Quantity"
+                  >
+        </div>
+        <div class="pull-right">
+          <button
+                  class="btn btn-success"
+                  >Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style>
+  a {
+    color: black;
+  }
+  .panel-panel-heading {
+    background: rgba(150,255,150);
+    padding: 10px 0 10px 10px;
+  }
+</style>
+
